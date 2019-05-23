0629d8e7169e8869b1a71ed65f241d7e4a2f6483
wess2kak <23626704+wess2kak@users.noreply.github.com>
2019-05-23 01:22:08 -0400

    fix indentation and rename variable that conflicted with builtin

--- network_activity.py
+++ network_activity.py
@@ -1,22 +1,21 @@
-def get_network_activity(interface, type):
- interface_name_length = len(interface)
- with open('/proc/net/dev', 'r') as f:
-  interface_present = False
-  for line in f.readlines():
-   if line.split()[0][:2] == interface:
-    interface_present = True
-  if not interface_present:
-   return print('Could not find interface: ' + interface)
+def get_network_activity(interface, rx_or_tx):
+    with open('/proc/net/dev', 'r') as f:
+        interface_present = False
+        for line in f.readlines():
+            if line.split()[0][:2] == interface:
+                interface_present = True
+        if not interface_present:
+            return print('Could not find interface: ' + interface)
 
-  rx_bytes = None
-  tx_bytes = None
- while True:
-  with open('/proc/net/dev', 'r') as f:
-   lines = list(f)
-   for line in lines:
-    if line[:2] == interface:
-     line_segments = line.split()
-     rx_bytes = int(line_segments[1])
-     tx_bytes = int(line_segments[9])
-     break
-   yield tx_bytes if type == 'tx' else rx_bytes
+        rx_bytes = None
+        tx_bytes = None
+    while True:
+        with open('/proc/net/dev', 'r') as f:
+            lines = list(f)
+            for line in lines:
+                if line[:2] == interface:
+                    line_segments = line.split()
+                    rx_bytes = int(line_segments[1])
+                    tx_bytes = int(line_segments[9])
+                    break
+            yield tx_bytes if rx_or_tx == 'tx' else rx_bytes

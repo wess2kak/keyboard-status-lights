87d1bef787251ca55633bd775370555a93216002
wess2kak <23626704+wess2kak@users.noreply.github.com>
2019-05-23 00:27:20 -0400

    search in other hwmon directories if they exist

--- temps.py
+++ temps.py
@@ -4,13 +4,16 @@ def get_cpu_temps():
  file_to_read = None
  if path.exists('/sys/class/thermal/thermal_zone0'):
   return # i don't have a computer with this
- elif path.exists('/sys/class/hwmon/hwmon0/'):
-  for directory in listdir('/sys/class/hwmon/hwmon0/'):
-   if directory.startswith('temp') and directory.endswith('label'):
-    with open('/sys/class/hwmon/hwmon0/' + directory, 'r') as f:
-     if f.read().startswith('Core 0'):
-      file_to_read = '/sys/class/hwmon/hwmon0/' + directory.split('_')[0] + '_input'
-      break
+ elif path.exists('/sys/class/hwmon/'):
+  for directory in listdir('/sys/class/hwmon/'):
+   with open('/sys/class/hwmon/' + directory + '/name', 'r') as f:
+    if f.read().rstrip() == 'coretemp':
+     for file in listdir('/sys/class/hwmon/' + directory):
+      if file.startswith('temp') and file.endswith('label'):
+       with open('/sys/class/hwmon/' + directory + '/' + file, 'r') as f:
+        if f.read().startswith('Core 0'):
+         file_to_read = '/sys/class/hwmon/hwmon0/' + file.split('_')[0] + '_input'
+         break
  if not file_to_read:
   return print('Could not find temperature interfaces.')
 

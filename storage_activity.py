4849c57e32bc0ae76e468b5ec92cf6f51ae61ac5
wess2kak <23626704+wess2kak@users.noreply.github.com>
2019-05-23 01:22:45 -0400

    fix indentation

--- storage_activity.py
+++ storage_activity.py
@@ -1,23 +1,22 @@
 def get_storage_activity(interface):
- storage_devices = []
- interface_present = False
- with open('/proc/diskstats', 'r') as f:
-  lines = list(f)
-  for line in lines:
-   storage_devices.append(line.split()[2])
-   if line.split()[2] == interface:
-    interface_present = True
-  if not interface_present:
-   return print('Could not find interface: ' + interface)
+    storage_devices = []
+    interface_present = False
+    with open('/proc/diskstats', 'r') as f:
+        lines = list(f)
+        for line in lines:
+            storage_devices.append(line.split()[2])
+            if line.split()[2] == interface:
+                interface_present = True
+        if not interface_present:
+            return print('Could not find interface: ' + interface)
 
- ms_spent_doing_io = 0
- while True:
-  with open('/proc/diskstats', 'r') as f:
-   lines = list(f)
-   for line in lines:
-    if line.split()[2] == interface:
-     line_segments = line.split()
-     ms_spent_doing_io = int(line_segments[12])
-     break
-   yield ms_spent_doing_io
-
+    ms_spent_doing_io = 0
+    while True:
+        with open('/proc/diskstats', 'r') as f:
+            lines = list(f)
+            for line in lines:
+                if line.split()[2] == interface:
+                    line_segments = line.split()
+                    ms_spent_doing_io = int(line_segments[12])
+                    break
+            yield ms_spent_doing_io

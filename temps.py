ec18c91e831467ab73e02d7595ffb84d83697af2
wess2kak <23626704+wess2kak@users.noreply.github.com>
2019-05-23 01:24:01 -0400

    fix indentation

--- temps.py
+++ temps.py
@@ -1,22 +1,23 @@
 from os import path, listdir
 
-def get_cpu_temps():
- file_to_read = None
- if path.exists('/sys/class/thermal/thermal_zone0'):
-  return # i don't have a computer with this
- elif path.exists('/sys/class/hwmon/'):
-  for directory in listdir('/sys/class/hwmon/'):
-   with open('/sys/class/hwmon/' + directory + '/name', 'r') as name_file:
-    if name_file.read().rstrip() == 'coretemp':
-     for file in listdir('/sys/class/hwmon/' + directory):
-      if file.startswith('temp') and file.endswith('label'):
-       with open('/sys/class/hwmon/' + directory + '/' + file, 'r') as f:
-        if f.read().startswith('Core 0'):
-         file_to_read = '/sys/class/hwmon/' + directory + '/' + file.split('_')[0] + '_input'
-         break
- if not file_to_read:
-  return print('Could not find temperature interfaces.')
 
- while True:
-  with open(file_to_read, 'r') as f:
-   yield int(int(f.read().lstrip()) / 1000)
+def get_cpu_temps():
+    file_to_read = None
+    if path.exists('/sys/class/thermal/thermal_zone0'):
+        return  # i don't have a computer with this
+    elif path.exists('/sys/class/hwmon/'):
+        for directory in listdir('/sys/class/hwmon/'):
+            with open('/sys/class/hwmon/' + directory + '/name', 'r') as name_file:
+                if name_file.read().rstrip() == 'coretemp':
+                    for file in listdir('/sys/class/hwmon/' + directory):
+                        if file.startswith('temp') and file.endswith('label'):
+                            with open('/sys/class/hwmon/' + directory + '/' + file, 'r') as f:
+                                if f.read().startswith('Core 0'):
+                                    file_to_read = '/sys/class/hwmon/' + directory + '/' + file.split('_')[0] + '_input'
+                                    break
+    if not file_to_read:
+        return print('Could not find temperature interfaces.')
+
+    while True:
+        with open(file_to_read, 'r') as f:
+            yield int(int(f.read().lstrip()) / 1000)
